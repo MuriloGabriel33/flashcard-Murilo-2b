6305f826bf5817c076857e9c831b663786b102c9
MuriloN401A <murilo.falchetti@escola.pr.gov.br>
2025-06-06 08:41:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisiculturismo',
+    'Quem é considerado o "pai do fisiculturismo moderno"?',
+    'Eugene Sandow é amplamente considerado o "pai do fisiculturismo moderno".'
+)
+
+criaCartao(
+    'Fisiculturismo',
+    'Qual o principal objetivo do fisiculturismo como esporte?',
+    'O principal objetivo é desenvolver a musculatura de forma equilibrada e simétrica, com alta definição e volume, para exibição em competições.'
+)
+
+criaCartao(
+    'Fisiculturismo',
+    'O que significa "cutting" e "bulking" no contexto do fisiculturismo?',
+    '"Bulking" é a fase de ganho de massa muscular (com ingestão calórica elevada), e "cutting" é a fase de perda de gordura corporal para definição muscular (com restrição calórica).'
+)
+
+criaCartao(
+    'Fisiculturismo',
+    'Quais são os principais grupos musculares trabalhados no fisiculturismo?',
+    'Os principais grupos são peito, costas, ombros, braços (bíceps e tríceps), pernas (quadríceps, isquiotibiais, panturrilhas) e abdômen.'
+)
